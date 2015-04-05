1c805f7cf72a5cf154bc7a20a42227c5ab15b5d4
Harold Pimentel <haroldpimentel@gmail.com>
2015-04-04 19:39:22 -0700

    output string to HDF5 using function

--- cpp/hdf5/src/h5make.cpp
+++ cpp/hdf5/src/h5make.cpp
@@ -10,9 +10,52 @@ std::unique_ptr<const char*> string_to_cptr(const std::vector<std::string>& v) {
   for (size_t i = 0; i < v.size(); ++i) {
     ret.get()[i] = v[i].c_str();
   }
-
   return std::move(ret);
 }
+
+// str_vec: a vector of string to be written out
+// group_id: a group_id which has already been opened
+// dataset_name: the to write out to
+// compression_level: the level of compression (6 seems reasonable)
+herr_t string_to_hdf5(
+    const std::vector<std::string>& str_vec,
+    hid_t group_id,
+    const std::string& dataset_name,
+    uint compression_level = 6) {
+  herr_t status;
+
+  hsize_t dims[1] = {str_vec.size()};
+
+  // create the data type
+  hid_t datatype_id = H5Tcopy (H5T_C_S1);
+  status = H5Tset_size (datatype_id, H5T_VARIABLE);
+
+  // create the propery which allows for compression
+  hid_t prop_id = H5Pcreate(H5P_DATASET_CREATE);
+  // chunk size is same size as vector
+  status = H5Pset_chunk(prop_id, 1, dims);
+  status = H5Pset_deflate(prop_id, compression_level);
+
+  // create the data space
+  hid_t dataspace_id = H5Screate_simple(1, dims, NULL);
+
+  // create the dataset
+  hid_t dataset_id = H5Dcreate(group_id, dataset_name.c_str(), datatype_id,
+      dataspace_id, H5P_DEFAULT, prop_id, H5P_DEFAULT);
+
+  // get the ptrs from the string and write out
+  auto cstr_ptr = string_to_cptr(str_vec);
+  status = H5Dwrite(dataset_id, datatype_id, dataspace_id, H5S_ALL, H5P_DEFAULT,
+      cstr_ptr.get());
+
+  H5Dclose(dataset_id);
+  H5Pclose(prop_id);
+  H5Sclose(dataspace_id);
+  H5Tclose(datatype_id);
+
+  return status;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -34,40 +77,39 @@ int main(int argc, char *argv[])
   }
 
   std::unique_ptr<const char*> out_ptr = string_to_cptr(str_vec);
-  char* out_str[] = {"hello", "goodbye", "what's up"};
   hid_t file_id;
   file_id = H5Fcreate(fname.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
   herr_t status;
   // get the right data type for variable len c strings
-  hid_t dtype = H5Tcopy (H5T_C_S1);
-  status = H5Tset_size (dtype, H5T_VARIABLE);
+
+  // hid_t dtype = H5Tcopy (H5T_C_S1);
+  // status = H5Tset_size (dtype, H5T_VARIABLE);
 
   hid_t root;
   root = H5Gopen(file_id, "/", H5P_DEFAULT);
 
-  hsize_t dims[1] = {NSTR};
+  // hsize_t dims[1] = {NSTR};
 
-  hid_t p_id = H5Pcreate(H5P_DATASET_CREATE);
-  status = H5Pset_chunk(p_id, 1, dims);
-  status = H5Pset_deflate(p_id, 6);
+  // hid_t p_id = H5Pcreate(H5P_DATASET_CREATE);
+  // status = H5Pset_chunk(p_id, 1, dims);
+  // status = H5Pset_deflate(p_id, 6);
 
-  hid_t data_space = H5Screate_simple(1, dims, NULL);
+  // hid_t data_space = H5Screate_simple(1, dims, NULL);
 
-  hid_t dset = H5Dcreate(root, "sup", dtype, data_space,
-      H5P_DEFAULT, p_id, H5P_DEFAULT);
   // hid_t dset = H5Dcreate(root, "sup", dtype, data_space,
-  //     H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-  std::cout << "hi!" << std::endl;
+  //     H5P_DEFAULT, p_id, H5P_DEFAULT);
+  // std::cout << "hi!" << std::endl;
 
-  /* status = H5Dwrite(dset, dtype, data_space, H5S_ALL, H5P_DEFAULT, out_str); */
-  status = H5Dwrite(dset, dtype, data_space, H5S_ALL, H5P_DEFAULT, out_ptr.get());
+  // status = H5Dwrite(dset, dtype, data_space, H5S_ALL, H5P_DEFAULT, out_ptr.get());
 
-  status = H5Dclose(dset);
+  status = string_to_hdf5(str_vec, root, "sup", 6);
+
+  /* status = H5Dclose(dset); */
   /* status = H5Aclose(att); */
   status = H5Gclose(root);
-  status = H5Tclose(dtype);
-  status = H5Sclose(data_space);
+  /* status = H5Tclose(dtype); */
+  /* status = H5Sclose(data_space); */
   status = H5Fclose(file_id);
 
   return 0;
--- cpp/hdf5/src/read.R
+++ cpp/hdf5/src/read.R
@@ -3,3 +3,6 @@ library("rhdf5")
 fname <- "../output.h5"
 h5ls(fname)
 h5read(fname, "/sup")
+
+fname2 <- "../output2.h5"
+h5read(fname2, "/sup")
