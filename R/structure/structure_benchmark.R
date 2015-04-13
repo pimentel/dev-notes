1f5bd987d49acc0d8d83494729bf2b270383bb56
Harold Pimentel <haroldpimentel@gmail.com>
2015-04-13 14:29:59 -0700

    add benchmark

--- R/structure/structure_benchmark.R
+++ R/structure/structure_benchmark.R
@@ -15,3 +15,8 @@ microbenchmark(
   create_class_class(),
   times = 1000L
   )
+
+# Unit: nanoseconds
+#                      expr  min   lq     mean median     uq   max neval
+#  create_class_structure() 5924 6490 7365.700 6909.0 7404.5 40591  1000
+#      create_class_class()  885 1004 1288.752 1103.5 1244.0 12787  1000
